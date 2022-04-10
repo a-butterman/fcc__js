5427c1bca82f6a04ab2aa32aab47034865a99bbc
a-butterman <apmyp.jezz@gmail.com>
2022-04-10 16:37:49 +0300

    added 85 lessons

---
+++ 01-basic-javascript/083.js
@@ -0,0 +1,9 @@
+const myDog = {
+    // Only change code below this line
+    name: "Guffy",
+    legs: 1,
+    tails: 2,
+    friends: ['Cat', 'Mouse']
+
+    // Only change code above this line
+};
