54736fba6aa6f6647f6eb725f565dfb157535c1b
a-butterman <apmyp.jezz@gmail.com>
2022-04-12 21:04:06 +0300

    added 103 lessons

---
+++ 01-basic-javascript/092.js
@@ -0,0 +1,15 @@
+const myMusic = [
+    {
+        "artist": "Billy Joel",
+        "title": "Piano Man",
+        "release_year": 1973,
+        "formats": ["CD", "8T", "LP"],
+        "gold": true
+    },
+    {
+        "artist": "Leningrad",
+        "title": "www",
+        "release_year": 1998,
+        "formats": ["CD", "8T", "LP"],
+    }
+];
